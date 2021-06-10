a58d80f114246f73854e571dc79142967360c396
FeuFeve <clement-potin@outlook.com>
2021-06-10 23:44:42 +0200

    Add copper ore and copper wire

--- src/main/java/com/feufeve/mccourse/block/ModBlocks.java
+++ src/main/java/com/feufeve/mccourse/block/ModBlocks.java
@@ -8,6 +8,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 
 import java.util.function.Supplier;
@@ -15,13 +16,19 @@ import java.util.function.Supplier;
 public class ModBlocks {
 
     public static final RegistryObject<Block> COPPER_BLOCK = register("copper_block",
-            () -> new Block(AbstractBlock.Properties.create(Material.IRON).
-                    hardnessAndResistance(3f, 10f).
-                    sound(SoundType.METAL)));
+            () -> new Block(AbstractBlock.Properties.create(Material.IRON)
+                    .hardnessAndResistance(3f, 10f)
+                    .sound(SoundType.METAL)));
+    public static final RegistryObject<Block> COPPER_ORE = register("copper_ore",
+            () -> new Block(AbstractBlock.Properties.create(Material.ROCK)
+                    .hardnessAndResistance(3f, 10f)
+                    .sound(SoundType.STONE)
+                    .harvestLevel(2)
+                    .harvestTool(ToolType.PICKAXE)));
 
     public static void register() { }
 
-    private static <T extends Block>RegistryObject<T> register(String name, Supplier<T> block) {
+    private static <T extends Block> RegistryObject<T> register(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = Registration.BLOCKS.register(name, block);
         Registration.ITEMS.register(name,
                 () -> new BlockItem(toReturn.get(), new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)));
--- src/main/java/com/feufeve/mccourse/item/ModItems.java
+++ src/main/java/com/feufeve/mccourse/item/ModItems.java
@@ -9,6 +9,8 @@ public class ModItems {
 
     public static final RegistryObject<Item> COPPER_INGOT = Registration.ITEMS.register("copper_ingot",
             () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
+    public static final RegistryObject<Item> COPPER_WIRE = Registration.ITEMS.register("copper_wire",
+            () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
 
     public static void register() { }
 }
